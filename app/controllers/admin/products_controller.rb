432178bf9bd96e2f17d097e4ff72295eaeae19b2
simplyDonald <donaldabuah@gmail.com>
2022-01-07 22:28:56 +0000

    use ENV file for username and password

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,5 +1,5 @@
 class Admin::ProductsController < ApplicationController
-  http_basic_authenticate_with name: "Jungle", password: "book"
+  http_basic_authenticate_with name: ENV["USERNAME"], password: ENV["PASSWORD"]
   
   
   def index
