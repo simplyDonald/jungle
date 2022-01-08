095d52cf1eb43382fc925f635838b659abfaf12e
simplyDonald <donaldabuah@gmail.com>
2022-01-08 01:41:31 +0000

    add category link to navbar and create category views page

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -4,5 +4,7 @@ class CategoriesController < ApplicationController
     @category = Category.find(params[:id])
     @products = @category.products.order(created_at: :desc)
   end
-
+  def index
+    @categories = Category.order(id: :desc).all
+  end
 end
